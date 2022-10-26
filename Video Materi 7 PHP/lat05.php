b4f409661c93bdcb77eac758c9a46553026489ec
feisa ramadhani <111243780+feisa11@users.noreply.github.com>
2022-10-26 08:22:40 +0700

    Add files via upload

---
+++ Video Materi 7 PHP/lat05.php
@@ -0,0 +1,28 @@
+<?php 
+
+$tanggal = 0;
+
+//$hasil = $tanggal > 32;
+
+if ($tanggal < 32) {
+    if ($tanggal > 0) {
+        echo 'Benar';
+    } else {
+        echo 'Salah';
+    }
+
+} else {
+    echo 'Salah';
+}
+
+$nilai = 1-1;
+
+if ($nilai <= 100){
+    if ($nilai >= 0) {
+        echo 'Nilai Benar';
+    }
+} else {
+    echo 'Nilai Salah';
+}
+
+?>

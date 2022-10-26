0694e247b4838cfc3f748c0b13ae53f1b796f0c2
feisa ramadhani <111243780+feisa11@users.noreply.github.com>
2022-10-26 08:28:38 +0700

    Add files via upload

---
+++ Video Materi 14 PHP/lat04.php
@@ -0,0 +1,52 @@
+<?php 
+
+    // OPERATOR MATEMATIKA
+
+    $a = 2;
+    $b = 5;
+
+    $c = $a + $b;
+
+    echo $c. '<br>';
+
+    $c = $a - $b;
+    echo $c. '<br>';
+
+    $c - $a * $b;
+    echo $c. '<br>';
+
+    $c - $a / $b;
+    echo floor($c).'<br>';
+
+    $c - $a % $b;
+    echo $c.'<BR>';
+
+    // OPERATOR LOGIKA
+
+    $c = $a < $b;
+    echo $c;
+
+    $c = $a > $b;
+    echo $c;
+
+    $c = $a == $b;
+    echo $c;
+
+    $c - $a != $b;
+    echo $c;
+
+    // INCREMENT
+    $a++;
+    echo  a;
+
+    // OPERATOR STRING
+
+    $kata = 'Sura';
+    $kota = 'Baya';
+
+    $hasil = $kata.$kota;
+
+    $hasil .=' KOTA PAHLAWAN ';
+    echo $hasil;
+
+?>
